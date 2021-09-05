735d6d633574f6d4e8361e70d3c80e9f0b6f657c
flycliff <wtc8754@rit.edu>
2021-09-05 10:31:14 -0400

    super basic api

---
+++ README.md
@@ -0,0 +1,8 @@
+# Kubernetes API Template & Practice
+
+Used while learning how to set up infrastructure and deploy a kubernetes cluster to different environments
+
+- Local
+- Dev (on-prem/manually configured server)
+- Staging/Prod (hosting provider - most likely AWS because i bought a domain for keeptorch.com)
+
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/google/jsonapi"
+
+	"github.com/Altruist-Motion/keeptorch/model"
+)
+
+func main() {
+	server := http.Server{
+		Addr: ":8080",
+		Handler: Router(),
+	}
+
+	shutdown := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Printf("API Shutdown - Error: %v\n", err)
+		}
+		close(shutdown)
+	}()
+
+	log.Printf("HTTP api listening on 8080\n")
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Printf("err: %v\n", err)
+	}
+
+	<-shutdown
+}
+
+func Router() chi.Router {
+	router := chi.NewRouter()
+
+	router.Use(middleware.StripSlashes)
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Recoverer)
+
+	router.Get("/get-hello-world", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("GET request from client\n")
+		SendResponseToClient(w, http.StatusOK, "Hello, world!")
+	})
+
+	router.Post("/post-hello-world", func(w http.ResponseWriter, r *http.Request) {	
+		var payload model.SamplePayload
+
+		bodyBytes := new(bytes.Buffer)
+		_, err := bodyBytes.ReadFrom(r.Body)
+	
+		if err != nil {
+			log.Printf("Error reading request body: %v\n", err)
+			SendResponseToClient(w, http.StatusInternalServerError, err)
+			return
+		}
+	
+		if err = payload.UnmarshalJSON(bodyBytes.Bytes()); err != nil {
+			log.Printf("Error unmarshalling payload: %v\n", err)
+			SendResponseToClient(w, http.StatusInternalServerError, err)
+			return
+		}
+	
+		log.Printf("POST request from client. Payload: %v\n", payload)
+	
+		SendResponseToClient(w, http.StatusNoContent, nil)
+	})
+
+	return router
+}
+
+type Success struct {
+	Data     interface{} `json:"data"` 
+}
+
+func SendResponseToClient(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", jsonapi.MediaType)
+	w.Header().Set("Cache-Control", "private, max-age=60")
+	w.WriteHeader(statusCode)
+
+	if statusCode == http.StatusNoContent {
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(Success{Data: data}); err != nil {
+		log.Println("error occurred while marshalling payload: ", err)
+	}
+}
